6e087b8423264df1d342d1d0104bf742af536828
Richard Russell <95770274+richard-russell@users.noreply.github.com>
2025-06-20 16:31:39 +0100

    HC Demo: Add locals.tf

---
+++ locals.tf
@@ -0,0 +1,8 @@
+  locals {
+  customer_name = var.customer_name != "" ? substr(var.customer_name, 0, 10) : "hashicat-inc"
+  default_tags = {
+    owner       = var.ddr_tfc_project_name
+    environment = var.env
+    customer    = local.customer_name
+  }
+}
